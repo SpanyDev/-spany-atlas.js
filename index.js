c2df67a44570336f638f7aa6c486c5c01aa62885
SpanyDev <spanyries@gmail.com>
2024-07-10 00:06:55 +0300

    Version Update

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { isValidHexColor } = require("./src/isHexColor");
 const { checkUpdate } = require("./src/checkUpdate");
 const { isImageUrl } = require("./src/isImageUrl");
+const { sonDepremler } = require("./src/deprem");
 
 module.exports = {
   isValidHexColor,
   checkUpdate,
   isImageUrl,
+  sonDepremler,
 };
--- src/deprem.js
+++ src/deprem.js
@@ -3,6 +3,10 @@ const axios = require('axios');
 async function sonDepremler(numberOfEarthquakes) {
     try {
         const response = await axios.get('https://api.orhanaydogdu.com.tr/deprem/kandilli/live');
+        if (isNaN(sonDepremler)) throw new Error('sonDepremler parametresi sayı olmalıdır!');
+        if (numberOfEarthquakes < 1) throw new Error('sonDepremler parametresi 1\'den küçük olamaz!');
+        if (numberOfEarthquakes > response.data.result.length) throw new Error('API\'dan dönen deprem sayısından fazla bir değer girdiniz!');
+
         const depremler = response.data.result.slice(0, numberOfEarthquakes);
 
         const translatedDepremler = depremler.map(deprem => {
