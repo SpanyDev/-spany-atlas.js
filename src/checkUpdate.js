49c5172bbcb061ce4a5585ed364b631d1a193450
SpanyDev <spanyries@gmail.com>
2024-07-09 18:22:15 +0300

    Version Update

--- src/checkUpdate.js
+++ src/checkUpdate.js
@@ -1,23 +1,37 @@
+const { exec } = require('child_process');
+const package = require('../package.json');
+
 async function checkUpdate() {
-  function getPackageVersion() {
-    return new Promise((resolve, reject) => {
-      exec(`npm view @spany/atlas.js version`, (error, stdout, stderr) => {
-        if (error) return { error };
-        if (stderr) return { error: stderr };
-        resolve({ version: stdout.trim(), error: false });
-      });
-    });
+  try {
+    const { stdout, stderr } = await executeCommand(`npm view @spany/atlas.js version`);
+
+    if (stderr) {
+      throw new Error(stderr);
+    }
+
+    const latestVersion = stdout.trim();
+    
+    if (latestVersion !== package.version) {
+      console.log(`\x1b[34m@spany/atlas.js is out of date!\x1b[0m`);
+      console.log(`Remember to use \x1b[32m"npm update @spany/atlas.js"\x1b[0m ( ${package.version} -> ${latestVersion} )`);
+    } else {
+      console.log(`\x1b[32m@spany/atlas.js is up to date.\x1b[0m`);
+    }
+  } catch (error) {
+    console.error(`Error checking for updates: ${error.message}`);
   }
+}
 
-  if (version.error) return;
-
-  if (version.version) {
-    if (version.version !== package.version) {
-      console.log(
-        `\x1b[34m@spany/atlas.js is out of date!\x1b[0m Remember to use \x1b[32m"npm update @spany/atlas.js" ( ${package.version} -> ${version.version} )\x1b[0m in powershell to take advantage of the new features.`
-      );
-    } else return;
-  } else return;
+function executeCommand(command) {
+  return new Promise((resolve, reject) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve({ stdout, stderr });
+      }
+    });
+  });
 }
 
 module.exports = { checkUpdate };
